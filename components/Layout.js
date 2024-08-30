0635f5fa143ba14b0937d6f8669fa26b1f6d85fd
Aryan Saxena <aryanwork926@gmail.com>
2024-08-30 17:45:41 +0530

    fourth

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useSession , signOut} from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 
 export default function Layout({ children }) {
-  const session = useSession();
+  const { data: session } = useSession();
+  let user = '';
+
+  if (session && session.user && session.user.name) {
+    user = session.user.name;
+  }
+
   return (
     <>
       <nav className="bg-[#015ca8] p-4">
         <div className="container mx-auto flex justify-between items-center">
           <p className="text-black">PropertyBooker</p>
-          <p className="text-black">Hello {session.data.user.name}</p> 
-          <buttpn onClick={signOut}>logut</buttpn>
+          <p className="text-black">Hello {user}</p>
+          <button onClick={signOut}>Logout</button>
           <Link href="/checkout" legacyBehavior>
             <a className="text-black">Checkout</a>
           </Link>
