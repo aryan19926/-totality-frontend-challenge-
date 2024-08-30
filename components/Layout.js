f747264eee54079a01e5786d6975707154c56ffa
Aryan Saxena <aryanwork926@gmail.com>
2024-08-30 18:14:00 +0530

    removed auth

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useSession, signOut } from 'next-auth/react';
 
 export default function Layout({ children }) {
-  const { data: session } = useSession();
-  let user = '';
-
-  if (session && session.user && session.user.name) {
-    user = session.user.name;
-  }
-
+  
   return (
     <>
       <nav className="bg-[#015ca8] p-4">
         <div className="container mx-auto flex justify-between items-center">
           <p className="text-black">PropertyBooker</p>
-          <p className="text-black">Hello {user}</p>
-          <button onClick={signOut}>Logout</button>
+          
+          
           <Link href="/checkout" legacyBehavior>
             <a className="text-black">Checkout</a>
           </Link>
